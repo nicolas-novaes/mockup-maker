d5119daa7fde20558441261e04628286a1072cef
Developer <dev@example.com>
2026-06-01 18:51:48 -0300

    Remove react-router - fix F5 redirect issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Editor } from './components/Editor';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Editor />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
-  );
+  return <Editor />;
 }
 
 export default App;
